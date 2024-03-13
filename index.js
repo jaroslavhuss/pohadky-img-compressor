4f6be00dfdd7eb2524fa4d5c4bbb94973ecea0dd
Jaroslav Huss <jaroslavhuss@jarduv-macbuk.home>
2024-03-13 14:15:17 +0100

    huuuH

---
+++ index.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const sharp = require('sharp');
+
+
+
+const main = () =>{
+//check if the output folder exists
+if(!fs.existsSync('./output')){
+    fs.mkdirSync('./output');
+}
+const files = fs.readdirSync(__dirname).filter((file) => file.endsWith(".png"));
+files.forEach(async (file) => {
+    const image = sharp(file);
+    image.resize(400, 400).png({ quality: 10 }).toFile(`./output/${file}`);
+  });
+}
+
+main()
