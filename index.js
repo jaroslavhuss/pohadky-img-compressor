60af9cecae32caab17b1047c321255b2dff15382
root <jaroslav.huss@gmail.com>
2025-03-26 08:26:44 +0000

    hmm

--- index.js
+++ index.js
@@ -11,9 +11,10 @@ if(!fs.existsSync('./output')){
 const files = fs.readdirSync(__dirname).filter((file) => file.endsWith(".png"));
 files.forEach(async (file) => {
     const image = sharp(file);
-    image.resize(400, 400).png({ quality: 10 }).toFile(`./output/${file}`);
+    const data = await image.resize(400, 400).png({ quality: 10 }).toFile(`./output/${file}`)
     //And delete the original file
     fs.unlinkSync(file);
+   
   });
 }
 
